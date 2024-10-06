01976e85862ce79e71021db513dbb1cc25bea773
Leon Kennedy <mikaelmrvital@gmail.com>
2024-10-06 15:23:29 -0300

    fix: buttons links and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import Hero from '../components/Hero';
 import About from '@/components/sections/About';
 import TeamContacts from '@/components/sections/TeaamContacts';
+import Explore from '@/components/sections/Explore';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Explore />
       <About />
       <TeamContacts />
     </>
