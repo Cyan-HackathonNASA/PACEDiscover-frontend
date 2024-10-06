780c9c463b7bfa5bca0d483c0e1f6c5b55b0b819
marcinhojazz <marcinhojazz@gmail.com>
2024-10-06 15:08:08 -0300

    updates cene

--- src/app/explorer-ocean/page.tsx
+++ src/app/explorer-ocean/page.tsx
@@ -12,30 +12,34 @@ export default function ExplorerOcean() {
 
   return (
     <section className="bg-gray-900 text-white">
-      <div className='min-h-screen flex flex-col gap-6 items-center justify-center px-20'>
-        <h1 className='font-semibold text-5xl w-full'>Explore the Ocean</h1>
-        <p className='leading-relaxed tracking-wide mt-2 text-lg'>
-          In this section, users will be able to navigate the Earth in an interactive 3D environment. By using filters, they can view specific data on algae and plankton in more detail. Additional information about each filter will be available to help with understanding.
-        </p>
+      <div className='min-h-screen flex flex-col lg:flex-row items-center justify-between lg:px-20 px-6'>
+        
+        {/* Seção de texto e botões */}
+        <div className='lg:w-1/2 flex flex-col gap-6 items-center justify-center lg:items-start'>
+          <h1 className='font-semibold text-5xl text-center lg:text-left w-full'>Explore the Ocean</h1>
+          <p className='leading-relaxed tracking-wide mt-2 text-lg text-center lg:text-left'>
+            In this section, users will be able to navigate the Earth in an interactive 3D environment. By using filters, they can view specific data on algae and plankton in more detail. Additional information about each filter will be available to help with understanding.
+          </p>
 
-        {/* Botões de navegação */}
-        {!show3D && (
-          <div className='w-full flex gap-4'>
-            <button
-              className='text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-lg px-5 py-2.5 text-center mb-2 transition-all duration-300 ease-in-out w-1/2'
-              onClick={() => setShow3D(true)}
-            >
-              Explore the interactive 3D ocean
-            </button>
-            <button className='text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-lg px-5 py-2.5 text-center mb-2 transition-all ease-in-out duration-300 w-1/2'>
-              Learn about PACE-OCI
-            </button>
-          </div>
-        )}
+          {/* Botões de navegação */}
+          {!show3D && (
+            <div className='w-full flex gap-4 mt-4'>
+              <button
+                className='text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-lg px-5 py-2.5 text-center mb-2 transition-all duration-300 ease-in-out w-1/2'
+                onClick={() => setShow3D(true)}
+              >
+                Explore the interactive 3D ocean
+              </button>
+              <button className='text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-lg px-5 py-2.5 text-center mb-2 transition-all ease-in-out duration-300 w-1/2'>
+                Learn about PACE-OCI
+              </button>
+            </div>
+          )}
+        </div>
 
         {/* Carregar a experiência 3D apenas após o clique */}
         {show3D && (
-          <div className='w-full h-screen z-50'>
+          <div className='lg:w-1/2 w-full h-screen lg:h-auto z-50 mt-10 lg:mt-0'>
             <EarthScene />
           </div>
         )}
